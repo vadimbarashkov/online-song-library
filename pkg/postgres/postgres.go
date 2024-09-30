7d1dee6fe2c38ccb49759cfa95a66517701b3fe0
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-09-30 17:54:24 +0300

    docs: write documentation for postgres package

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -20,32 +20,38 @@ const (
 	uniqueViolationErrCode = "23505"
 )
 
+// Option represents a functional option for configuring the database connection.
 type Option func(*sqlx.DB)
 
+// WithConnMaxIdleTime sets the maximum idle time for a connection in the pool.
 func WithConnMaxIdleTime(d time.Duration) Option {
 	return func(db *sqlx.DB) {
 		db.SetConnMaxIdleTime(d)
 	}
 }
 
+// WithConnMaxLifetime sets the maximum lifetime of a connection before it is closed.
 func WithConnMaxLifetime(d time.Duration) Option {
 	return func(db *sqlx.DB) {
 		db.SetConnMaxLifetime(d)
 	}
 }
 
+// WithMaxIdleConns sets the maximum number of idle connections allowed in the pool.
 func WithMaxIdleConns(n int) Option {
 	return func(db *sqlx.DB) {
 		db.SetMaxIdleConns(n)
 	}
 }
 
+// WithMaxOpenConns sets the maximum number of open connections in the pool.
 func WithMaxOpenConns(n int) Option {
 	return func(db *sqlx.DB) {
 		db.SetMaxOpenConns(n)
 	}
 }
 
+// New creates a new connection to the PostgreSQL database and applies the provided configuration options.
 func New(ctx context.Context, dsn string, opts ...Option) (*sqlx.DB, error) {
 	const op = "postgres.New"
 
@@ -66,6 +72,8 @@ func New(ctx context.Context, dsn string, opts ...Option) (*sqlx.DB, error) {
 	return db, nil
 }
 
+// IsUniqueViolationError checks if the provided error is a unique constraint violation error from
+// the PostgreSQL database. It returns true if the error is a unique violation, otherwise false.
 func IsUniqueViolationError(err error) bool {
 	pgErr, ok := err.(*pgconn.PgError)
 	return ok && pgErr.SQLState() == uniqueViolationErrCode
